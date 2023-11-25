bae079bdf0a3517b18d27606341fc690fc4f14ce
Max <karlssonmax2@gmail.com>
2023-11-25 12:38:47 +0100

    Skapade en scratchpad, importerat rätt bibliotek

---
+++ scratchpad.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Planen är att ta in data och sedan måste vi sortera ut beroende på preferenser
+# Testar att skicka upp scratchpad.py till git repositoryn nu
