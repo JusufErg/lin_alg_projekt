fe162774580c02a943af9cb3c20d8cbdceaf7cd5
Max <karlssonmax2@gmail.com>
2023-11-26 14:15:59 +0100

    Lagt till lite tankar

--- scratchpad.py
+++ scratchpad.py
@@ -3,3 +3,9 @@ import matplotlib.pyplot as plt
 
 # Planen är att ta in data och sedan måste vi sortera ut beroende på preferenser
 # Testar att skicka upp scratchpad.py till git repositoryn nu
+
+# Vad som behöver göras är en ekvationslösare där krav på ekvationer såsom
+# x + y = k
+# x + y < k
+# x + y > k
+# kan ställas på systemet
